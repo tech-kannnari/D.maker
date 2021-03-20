660c781c422f1e334b1123d5f5ad7859f1a31f44
tech-kannnari <madpowertk@gmail.com>
2021-03-20 17:43:59 +0900

    細かい微調整

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -22,3 +22,5 @@ class MenusController < ApplicationController
   def menu_params
     params.require(:menu).permit(:main_name, :main_recipe, :main_genre, :salad_name, :salad_recipe, :salad_genre, :soup_name, :soup_recipe, :soup_genre)
   end
+
+end
