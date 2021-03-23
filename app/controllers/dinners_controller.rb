2e8efff159ef03c748320fb8f9be0a392df070c5
tech-kannnari <madpowertk@gmail.com>
2021-03-23 12:28:49 +0900

    ランダムでメニューを出力

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,4 +1,15 @@
 class DinnersController < ApplicationController
   def index
+    @random = Menu.order("RAND()").limit(1)
+
   end
+
+  private
+
+  def dinner_params
+    params.require(:dinner).permit(:image, :impression, :cookday, :cooktime)
+  end
+
 end
+
+
