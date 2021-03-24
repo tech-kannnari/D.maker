332557e5d9c30c9ab8347cae028b0abafded3c4a
tech-kannnari <madpowertk@gmail.com>
2021-03-24 16:12:01 +0900

    バリデーションの修正

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -2,5 +2,9 @@ class Dinner < ApplicationRecord
   # belongs_to :menu
   has_one_attached :image
 
-  validates :impression, presence: true
+  with_options presence: true do
+    validates :impression
+    validates :image
+    validates :cookday
+  end    
 end
